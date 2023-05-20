22ff1f6f245b0328401ecddf90a35e9b3a96106e
MaateusMDS <maateusmania@gmail.com>
2023-05-19 21:35:39 -0300

    refactoring controller folder

--- src/main/java/com/nike/controller/GptController.java
+++ src/main/java/com/nike/controller/GptController.java
@@ -1,20 +1,17 @@
 package com.nike.controller;
 
-import com.nike.model.record.criarUser;
-import com.nike.model.record.getChat;
 import com.nike.gpt.Main;
+import com.nike.model.record.getChat;
 import org.json.JSONObject;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping
-public class Controller {
+public class GptController {
 
     @PostMapping("/chat")
     public static String chat(@RequestBody getChat request) {
@@ -24,13 +21,6 @@ public class Controller {
         JSONObject systemMessage = new JSONObject().put("role", "system").put("content", "Você é um assistente comediante");
         messages.add(systemMessage);
 
-            return Main.chat(request.mensagem(), messages);
+        return Main.chat(request.mensagem(), messages);
     }
-
-    @PostMapping("/user")
-    public static String criarUsuario(@RequestBody criarUser dados){
-        return "";
-
-    }
-
 }
